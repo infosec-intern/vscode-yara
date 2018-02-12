ed47cb83d640ed1b83c279eec5ffca4bf8c4e7a4
Thomas Gardner <tpgardner19@gmail.com>
2018-02-11 20:12:37 -0700

    slowly working through definition providers

--- yara/src/extension.ts
+++ yara/src/extension.ts
@@ -8,14 +8,13 @@ import * as vscode from "vscode";
 class YaraDefinitionProvider implements vscode.DefinitionProvider {
     public provideDefinition(doc: vscode.TextDocument, pos: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.Location> {
         return new Promise((resolve, reject) => {
-            const fileUri = vscode.Uri.file(doc.fileName);
-            // // grab the character range for the current symbol to define
-            const range = doc.getWordRangeAtPosition(pos);
-            // // grab the actual symbol to define
-            const symbol = doc.getText(range);
-            // // assume the symbol isn't defined anywhere
-            let results = new vscode.Location(fileUri, range);
-            if (false) {
+            // const fileUri: vscode.Uri = vscode.Uri.file(doc.fileName);
+            const range: vscode.Range = doc.getWordRangeAtPosition(pos);
+            const symbol: string = doc.getText(range);
+            console.log(`Providing definition for symbol '${symbol}'`);
+            let results = null;
+            console.log(`${JSON.stringify(results)}`);
+            if (results != null) {
                 resolve(results);
             }
             else {
@@ -27,11 +26,9 @@ class YaraDefinitionProvider implements vscode.DefinitionProvider {
 
 class YaraReferenceProvider implements vscode.ReferenceProvider {
     public provideReferences(doc: vscode.TextDocument, pos: vscode.Position, options: { includeDeclaration: boolean }, token: vscode.CancellationToken): Thenable<vscode.Location[]> {
-        const filename = vscode.Uri.file(doc.fileName);
-        const range = doc.getWordRangeAtPosition(pos);
-        const symbol = doc.getText(range);
-        // assume the symbol isn't defined anywhere
-        let results = new vscode.Location(filename, range);
+        const range: vscode.Range = doc.getWordRangeAtPosition(pos);
+        const symbol: string = doc.getText(range);
+        console.log(`Providing references for symbol '${symbol}'`);
         return null;
     }
 }
