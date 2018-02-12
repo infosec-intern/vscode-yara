184a197dcce99cbe0b11e2f26b67a6f66674e19a
Thomas Gardner <tpgardner19@gmail.com>
2018-02-11 21:33:22 -0700

    got rule definitions working

--- yara/src/extension.ts
+++ yara/src/extension.ts
@@ -8,14 +8,28 @@ import * as vscode from "vscode";
 class YaraDefinitionProvider implements vscode.DefinitionProvider {
     public provideDefinition(doc: vscode.TextDocument, pos: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.Location> {
         return new Promise((resolve, reject) => {
-            // const fileUri: vscode.Uri = vscode.Uri.file(doc.fileName);
+            let definition: vscode.Location = null;
+            const fileUri: vscode.Uri = vscode.Uri.file(doc.fileName);
             const range: vscode.Range = doc.getWordRangeAtPosition(pos);
             const symbol: string = doc.getText(range);
             console.log(`Providing definition for symbol '${symbol}'`);
-            let results = null;
-            console.log(`${JSON.stringify(results)}`);
-            if (results != null) {
-                resolve(results);
+            let lines: string[] = doc.getText().split("\n");
+            let lineNo = 0;
+            lines.forEach(line => {
+                let character: number = line.indexOf(symbol);
+                // line numbers are offset by one in output - need to adjust
+                // only supporting definitions for rules
+                if (character != -1 && (lineNo+1) != pos.line && line.startsWith("rule")) {
+                    console.log(`Found ${symbol} on line ${lineNo} at character ${character}`);
+                    let defPosition: vscode.Position = new vscode.Position(lineNo, character);
+                    definition = new vscode.Location(fileUri, defPosition);
+                    // Definition found. Break out of forEach
+                    return;
+                }
+                lineNo++;
+            });
+            if (definition != null) {
+                resolve(definition);
             }
             else {
                 reject();
