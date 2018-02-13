cb8867a94d4ed3bb585ef22a0d9df76dc2616d68
Thomas Gardner <tpgardner19@gmail.com>
2018-02-12 20:17:16 -0700

    filled out more of unit tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -16,6 +16,12 @@ suite("YARA: Provider", function() {
     test("rule definition", function(done) {
         const filepath: string = path.join(workspace, "peek_rules.yara");
         vscode.workspace.openTextDocument(filepath).then(function(doc) {
+            let defProvider: vscode.DefinitionProvider = new yara.YaraDefinitionProvider();
+            // SyntaxExample: Line 40, Col 14
+            let pos: vscode.Position = new vscode.Position(39, 14);
+            let tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
+            let definition = defProvider.provideDefinition(doc, pos, tokenSource.token);
+            console.log(`definition: ${JSON.stringify(definition)}`);
             done();
         });
     });
@@ -23,6 +29,13 @@ suite("YARA: Provider", function() {
     test("rule references", function(done) {
         const filepath: string = path.join(workspace, "peek_rules.yara");
         vscode.workspace.openTextDocument(filepath).then(function(doc) {
+            let refProvider: vscode.ReferenceProvider = new yara.YaraReferenceProvider();
+            // $dstring: Line 22, Col 11
+            let pos: vscode.Position = new vscode.Position(21, 11);
+            let ctx: vscode.ReferenceContext = null;
+            let tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
+            let references = refProvider.provideReferences(doc, pos, ctx, tokenSource.token);
+            console.log(`references: ${JSON.stringify(references)}`);
             done();
         });
     });
