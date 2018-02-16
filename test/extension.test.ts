4e83e9e63ed3724a24cdf21c8c227c0a89b3954f
Thomas Gardner <tpgardner19@gmail.com>
2018-02-15 18:44:45 -0700

    added test "issue #17"

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -5,7 +5,6 @@ Note: This example test is leveraging the Mocha test framework.
 Please refer to their documentation on https://mochajs.org/ for help.
 */
 
-import * as assert from "assert";
 import * as path from "path";
 import * as vscode from "vscode";
 import * as yara from "../yara/src/extension";
@@ -71,6 +70,51 @@ suite("YARA: Provider", function () {
                             let refWordRange = doc.getWordRangeAtPosition(reference.range.start);
                             let refWord: string = doc.getText(refWordRange);
                             if (refWord != "dstring") { passed = false; }
+                        });
+                        if (passed) { done(); }
+                    }
+                });
+            }
+        });
+    });
+
+    /*
+        Trying to capture $hex_string but not $hex_string2
+        Should collect references for:
+            $hex_string = { E2 34 ?? C8 A? FB [2-4] }
+            $hex_string
+        But not:
+            $hex_string2 = { F4 23 ( 62 B4 | 56 ) 45 }
+    */
+    test("issue #17", function (done) {
+        const filepath: string = path.join(workspace, "peek_rules.yara");
+        vscode.workspace.openTextDocument(filepath).then(function (doc) {
+            let refProvider: vscode.ReferenceProvider = new yara.YaraReferenceProvider();
+            // $hex_string: Line 20, Col 11
+            let pos: vscode.Position = new vscode.Position(20, 11);
+            // console.log(`search term: ${doc.getText(doc.getWordRangeAtPosition(pos))}`);
+            let ctx: vscode.ReferenceContext = null;
+            let tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
+            let results = refProvider.provideReferences(doc, pos, ctx, tokenSource.token);
+            let passed: boolean = true;
+            if (results instanceof Array && results.length == 2) {
+                results.forEach(reference => {
+                    let refWordRange: vscode.Range = doc.getWordRangeAtPosition(reference.range.start);
+                    let refWord: string = doc.getText(refWordRange);
+                    if (refWord != "hex_string") { passed = false; }
+                });
+                if (passed) { done(); }
+            }
+            else if (results instanceof Promise) {
+                results.then(function(references) {
+                    if (references.length != 2) {
+                        passed = false;
+                    }
+                    else {
+                        references.forEach(reference => {
+                            let refWordRange = doc.getWordRangeAtPosition(reference.range.start);
+                            let refWord: string = doc.getText(refWordRange);
+                            if (refWord != "hex_string") { passed = false; }
                         });
                         if (passed) { done(); }
                     }
