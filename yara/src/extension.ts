a09187b0d1a3f9bc1111240bc29860a081905bf7
Thomas Gardner <tpgardner19@gmail.com>
2018-02-17 22:16:41 -0700

    created GetRuleRange()

--- yara/src/extension.ts
+++ yara/src/extension.ts
@@ -6,6 +6,31 @@ import * as vscode from "vscode";
 // variables have a few possible first characters - use these to identify vars vs. rules
 const varFirstChar = new Set(["$", "#", "@", "!"]);
 
+/*
+    Get the start and end boundaries for the current YARA rule based on a symbol's position
+*/
+function GetRuleRange(lines: string[], symbol: vscode.Position) {
+    let begin: vscode.Position = null;
+    let end: vscode.Position = null;
+    const startRuleRegexp = RegExp("^rule ");
+    const endRuleRegexp = RegExp("^\}");
+    // only go up to the symbol's line because the rule must be defined before the symbol
+    for (let lineNo = 0; lineNo < symbol.line; lineNo++) {
+        if (startRuleRegexp.test(lines[lineNo])) {
+            begin = new vscode.Position(lineNo, 0);
+        }
+    }
+    // start up this loop again using the beginning of the rule
+    // and find the line with just a curly brace to identify the end of a rule
+    for (let lineNo = begin.line; lineNo < lines.length; lineNo++) {
+        if (endRuleRegexp.test(lines[lineNo])) {
+            end = new vscode.Position(lineNo, 0);
+            break;
+        }
+    }
+    return new vscode.Range(begin, end);
+}
+
 export class YaraDefinitionProvider implements vscode.DefinitionProvider {
     public provideDefinition(doc: vscode.TextDocument, pos: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.Location> {
         return new Promise((resolve, reject) => {
@@ -17,40 +42,21 @@ export class YaraDefinitionProvider implements vscode.DefinitionProvider {
             let possibleVarStart: vscode.Position = new vscode.Position(range.start.line, range.start.character-1);
             let possibleVarRange: vscode.Range = new vscode.Range(possibleVarStart, range.end);
             let possibleVar: string = doc.getText(possibleVarRange);
+            const lines: string[] = doc.getText().split("\n");
             if (varFirstChar.has(possibleVar.charAt(0))) {
                 // console.log(`Variable detected: ${possibleVar}`);
-                const lines: string[] = doc.getText().split("\n");
-                let currRuleBegin: number = 0;
-                let currRuleEnd: number = 0;
-                const startRuleRegexp = RegExp("^rule ");
-                const endRuleRegexp = RegExp("^\}");
-                // only go up to the symbol's line because the rule must be
-                // defined before the symbol
-                for (let lineNo = 0; lineNo < pos.line; lineNo++) {
-                    if (startRuleRegexp.test(lines[lineNo])) {
-                        currRuleBegin = lineNo;
-                    }
-                }
-                // start up this loop again using the beginning of the rule
-                // and find the line with just a curly brace to identify the end of a rule
-                for (let lineNo = currRuleBegin; lineNo < lines.length; lineNo++) {
-                    if (endRuleRegexp.test(lines[lineNo])) {
-                        currRuleEnd = lineNo;
-                        break;
-                    }
-                }
-                console.log(`Curr rule range: ${currRuleBegin} -> ${currRuleEnd}`);
-                for (let lineNo = currRuleBegin; lineNo < currRuleEnd; lineNo++) {
+                let currentRuleRange: vscode.Range = GetRuleRange(lines, pos);
+                // console.log(`Curr rule range: ${currentRuleRange.start.line+1} -> ${currentRuleRange.end.line+1}`);
+                for (let lineNo = currentRuleRange.start.line; lineNo < currentRuleRange.end.line; lineNo++) {
                     let character: number = lines[lineNo].indexOf(`$${symbol} =`);
                     if (character != -1) {
-                        console.log(`Found defintion of '${possibleVar}' on line ${lineNo} at character ${character}`);
+                        // console.log(`Found defintion of '${possibleVar}' on line ${lineNo+1} at character ${character+1}`);
                         let defPosition: vscode.Position = new vscode.Position(lineNo, character);
                         definition = new vscode.Location(fileUri, defPosition);
                     }
                 }
             }
             else {
-                let lines: string[] = doc.getText().split("\n");
                 let lineNo = 0;
                 lines.forEach(line => {
                     let character: number = line.indexOf(symbol);
