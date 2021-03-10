7af1ba25a07eaf29fd8ec0a656857cb029936596
infosec-intern <thomas@infosec-intern.com>
2021-03-10 09:35:06 -0700

    linting

--- yara/src/extension.ts
+++ yara/src/extension.ts
@@ -6,20 +6,20 @@ import {YaraDefinitionProvider} from "./definitionProvider";
 import {YaraReferenceProvider} from "./referenceProvider";
 
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
     // console.log("Activating Yara extension");
-    let YARA: vscode.DocumentSelector = { language: "yara", scheme: "file" };
-    let definitionDisposable: vscode.Disposable = vscode.languages.registerDefinitionProvider(YARA, new YaraDefinitionProvider());
-    let referenceDisposable: vscode.Disposable = vscode.languages.registerReferenceProvider(YARA, new YaraReferenceProvider());
-    let completionDisposable: vscode.Disposable = vscode.languages.registerCompletionItemProvider(YARA, new YaraCompletionItemProvider(), '.');
+    const YARA: vscode.DocumentSelector = { language: "yara", scheme: "file" };
+    const definitionDisposable: vscode.Disposable = vscode.languages.registerDefinitionProvider(YARA, new YaraDefinitionProvider());
+    const referenceDisposable: vscode.Disposable = vscode.languages.registerReferenceProvider(YARA, new YaraReferenceProvider());
+    const completionDisposable: vscode.Disposable = vscode.languages.registerCompletionItemProvider(YARA, new YaraCompletionItemProvider(), '.');
     context.subscriptions.push(definitionDisposable);
     context.subscriptions.push(referenceDisposable);
     context.subscriptions.push(completionDisposable);
-};
+}
 
-export function deactivate(context: vscode.ExtensionContext) {
+export function deactivate(context: vscode.ExtensionContext): void {
     // console.log("Deactivating Yara extension");
     context.subscriptions.forEach(disposable => {
         disposable.dispose();
     });
-};
+}
--- yara/src/referenceProvider.ts
+++ yara/src/referenceProvider.ts
@@ -5,44 +5,44 @@ import {GetRuleRange, varFirstChar} from "./helpers";
 
 
 export class YaraReferenceProvider implements vscode.ReferenceProvider {
-    public provideReferences(doc: vscode.TextDocument, pos: vscode.Position, options: { includeDeclaration: boolean }, token: vscode.CancellationToken): Thenable<vscode.Location[]> {
+    public provideReferences(doc: vscode.TextDocument, pos: vscode.Position): Thenable<vscode.Location[]> {
         return new Promise((resolve, reject) => {
             const fileUri: vscode.Uri = vscode.Uri.file(doc.fileName);
             const range: vscode.Range = doc.getWordRangeAtPosition(pos);
-            let lines: string[] = doc.getText().split("\n");
-            let references: vscode.Location[] = new Array<vscode.Location>();
-            let symbol: string = doc.getText(range);
+            const lines: string[] = doc.getText().split("\n");
+            const references: vscode.Location[] = new Array<vscode.Location>();
+            const symbol: string = doc.getText(range);
             // console.log(`Providing references for symbol '${symbol}'`);
-            let possibleVarStart: vscode.Position = new vscode.Position(range.start.line, range.start.character - 1);
-            let possibleVarRange: vscode.Range = new vscode.Range(possibleVarStart, range.end);
-            let possibleVar: string = doc.getText(possibleVarRange);
+            const possibleVarStart: vscode.Position = new vscode.Position(range.start.line, range.start.character - 1);
+            const possibleVarRange: vscode.Range = new vscode.Range(possibleVarStart, range.end);
+            const possibleVar: string = doc.getText(possibleVarRange);
             if (varFirstChar.has(possibleVar.charAt(0))) {
                 let varRegexp: string;
                 let startLine: number;
                 let endLine: number;
                 // console.log(`Identified symbol as a variable: ${symbol}`);
-                let possibleWildcardEnd: vscode.Position = new vscode.Position(range.end.line, range.end.character + 1);
-                let possibleWildcardRange: vscode.Range = new vscode.Range(possibleVarStart, possibleWildcardEnd);
-                let possibleWildcard: string = doc.getText(possibleWildcardRange);
+                const possibleWildcardEnd: vscode.Position = new vscode.Position(range.end.line, range.end.character + 1);
+                const possibleWildcardRange: vscode.Range = new vscode.Range(possibleVarStart, possibleWildcardEnd);
+                const possibleWildcard: string = doc.getText(possibleWildcardRange);
                 if (possibleWildcard.slice(-1) == "*") {
                     // treat like a wildcard and search only the local rule
-                    varRegexp = `[\$#@!]${symbol}[a-zA-Z0-9_]+`;
-                    let ruleRange = GetRuleRange(lines, pos);
+                    varRegexp = `[$#@!]${symbol}[a-zA-Z0-9_]+`;
+                    const ruleRange = GetRuleRange(lines, pos);
                     startLine = ruleRange.start.line;
                     endLine = ruleRange.end.line;
                 }
                 else {
                     // treat like a normal variable reference and search the whole document
-                    varRegexp = `[\$#@!](${symbol}[^a-zA-Z0-9_]|${symbol}$)`;
+                    varRegexp = `[$#@!](${symbol}[^a-zA-Z0-9_]|${symbol}$)`;
                     startLine = 0;
                     endLine = lines.length;
                 }
                 for (let lineNo = startLine; lineNo < endLine; lineNo++) {
-                    let character: number = lines[lineNo].search(varRegexp);
+                    const character: number = lines[lineNo].search(varRegexp);
                     if (character != -1) {
                         // console.log(`Found ${symbol} on line ${lineNo} at character ${character}`);
                         // have to readjust the character index
-                        let refPosition: vscode.Position = new vscode.Position(lineNo, character + 1);
+                        const refPosition: vscode.Position = new vscode.Position(lineNo, character + 1);
                         references.push(new vscode.Location(fileUri, refPosition));
                     }
 
@@ -51,10 +51,10 @@ export class YaraReferenceProvider implements vscode.ReferenceProvider {
             else {
                 let lineNo = 0;
                 lines.forEach(line => {
-                    let character: number = line.indexOf(symbol);
+                    const character: number = line.indexOf(symbol);
                     if (character != -1) {
                         // console.log(`Found ${symbol} on line ${lineNo} at character ${character}`);
-                        let refPosition: vscode.Position = new vscode.Position(lineNo, character);
+                        const refPosition: vscode.Position = new vscode.Position(lineNo, character);
                         references.push(new vscode.Location(fileUri, refPosition));
                     }
                     lineNo++;
