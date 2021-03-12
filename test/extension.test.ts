e94b6c61d57d0e0f6637c2f373d2cdba8d5d5dde
infosec-intern <thomas@infosec-intern.com>
2021-03-12 08:32:45 -0700

    updated all tests to use assert and executecommand

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -5,10 +5,9 @@ Note: This example test is leveraging the Mocha test framework.
 Please refer to their documentation on https://mochajs.org/ for help.
 */
 
+import * as assert from "assert";
 import * as path from "path";
 import * as vscode from "vscode";
-import { YaraDefinitionProvider } from "../yara/src/definitionProvider";
-import { YaraReferenceProvider } from "../yara/src/referenceProvider";
 
 
 const workspace = path.join(__dirname, "..", "..", "test/rules/");
@@ -16,127 +15,73 @@ const workspace = path.join(__dirname, "..", "..", "test/rules/");
 suite("YARA: Provider", function () {
     test("rule definition", function (done) {
         const filepath: string = path.join(workspace, "peek_rules.yara");
-        vscode.workspace.openTextDocument(filepath).then(function (doc) {
-            const defProvider: vscode.DefinitionProvider = new YaraDefinitionProvider();
-            // SyntaxExample: Line 43, Col 14
-            // line numbers start at 0, so we have to subtract one for the lookup
-            const pos: vscode.Position = new vscode.Position(42, 14);
-            const tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
-            const result = defProvider.provideDefinition(doc, pos, tokenSource.token);
-            if (result instanceof vscode.Location) {
-                const resultWordRange: vscode.Range = doc.getWordRangeAtPosition(result.range.start);
-                const resultWord: string = doc.getText(resultWordRange);
-                if (resultWord == "SyntaxExample") { done(); }
-            }
-            else if (result instanceof Array) {
-                // Should only get one result, so we've failed if an Array is returned
-            }
-            else if (result instanceof Promise) {
-                result.then(function (definition: vscode.Location) {
-                    const resultWordRange: vscode.Range = doc.getWordRangeAtPosition(definition.range.start);
-                    const resultWord: string = doc.getText(resultWordRange);
-                    if (resultWord == "SyntaxExample" && definition.range.start.line == 5) { done(); }
+        const uri: vscode.Uri = vscode.Uri.file(filepath);
+        // SyntaxExample: Line 43, Col 14
+        // line numbers start at 0, so we have to subtract one for the lookup
+        const pos: vscode.Position = new vscode.Position(42, 14);
+        vscode.commands.executeCommand("vscode.executeDefinitionProvider", uri, pos).then((definitions: vscode.Location[]) => {
+                console.log(`rule definitions: ${JSON.stringify(definitions)}`);
+                assert.equal(definitions.length, 1);
+                definitions.forEach((definition: vscode.Location) => {
+                    assert.equal(definition.uri.path, filepath);
+                    assert.equal(definition.range.start.line, 5);
                 });
-            }
-        });
+                done();
+            });
     });
 
     test("variable definition", function (done) {
         const filepath: string = path.join(workspace, "peek_rules.yara");
-        vscode.workspace.openTextDocument(filepath).then(function (doc) {
-            const defProvider: vscode.DefinitionProvider = new YaraDefinitionProvider();
-            // $hex_string: Line 25, Col 14
-            // line numbers start at 0, so we have to subtract one for the lookup
-            const pos: vscode.Position = new vscode.Position(24, 14);
-            const tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
-            const result = defProvider.provideDefinition(doc, pos, tokenSource.token);
-            if (result instanceof vscode.Location) {
-                const resultWordRange: vscode.Range = doc.getWordRangeAtPosition(result.range.start);
-                const resultWord: string = doc.getText(resultWordRange);
-                if (resultWord == "hex_string") { done(); }
-            }
-            else if (result instanceof Array) {
-                // Should only get one result, so we've failed if an Array is returned
-            }
-            else if (result instanceof Promise) {
-                result.then(function (definition: vscode.Location) {
-                    const resultWordRange: vscode.Range = doc.getWordRangeAtPosition(definition.range.start);
-                    const resultWord: string = doc.getText(resultWordRange);
-                    if (resultWord == "hex_string") { done(); }
-                });
-            }
+        const uri: vscode.Uri = vscode.Uri.file(filepath);
+        // $hex_string: Line 25, Col 14
+        // line numbers start at 0, so we have to subtract one for the lookup
+        const pos: vscode.Position = new vscode.Position(24, 14);
+        vscode.commands.executeCommand("vscode.executeDefinitionProvider", uri, pos).then((definitions: vscode.Location[]) => {
+            // console.log(`variable definitions: ${JSON.stringify(definitions)}`);
+            assert.equal(definitions.length, 1);
+            definitions.forEach((definition: vscode.Location) => {
+                assert.equal(definition.uri.path, filepath);
+                assert.equal(definition.range.start.line, 19);
+            });
+            done();
         });
     });
 
     test("symbol references", function (done) {
         const filepath: string = path.join(workspace, "peek_rules.yara");
-        vscode.workspace.openTextDocument(filepath).then(function (doc) {
-            const refProvider: vscode.ReferenceProvider = new YaraReferenceProvider();
-            // $dstring: Line 22, Col 11
-            const pos: vscode.Position = new vscode.Position(21, 11);
-            // console.log(`search term: ${doc.getText(doc.getWordRangeAtPosition(pos))}`);
-            const ctx: vscode.ReferenceContext | null = null;
-            const tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
-            const results = refProvider.provideReferences(doc, pos, ctx, tokenSource.token);
-            let passed = true;
-            const acceptableLines: Set<number> = new Set([21, 28, 29]);
-            if (results instanceof Array && results.length == 3) {
-                results.forEach(reference => {
-                    const refWordRange: vscode.Range = doc.getWordRangeAtPosition(reference.range.start);
-                    const refWord: string = doc.getText(refWordRange);
-                    if (refWord != "dstring" || !acceptableLines.has(reference.range.start.line)) { passed = false; }
-                });
-                if (passed) { done(); }
-            }
-            else if (results instanceof Promise) {
-                results.then(function (references) {
-                    if (references.length != 3) {
-                        passed = false;
-                    }
-                    else {
-                        references.forEach(reference => {
-                            const refWordRange = doc.getWordRangeAtPosition(reference.range.start);
-                            const refWord: string = doc.getText(refWordRange);
-                            if (refWord != "dstring" || !acceptableLines.has(reference.range.start.line)) { passed = false; }
-                        });
-                        if (passed) { done(); }
-                    }
-                });
-            }
+        const uri: vscode.Uri = vscode.Uri.file(filepath);
+        // $dstring: Line 22, Col 11
+        const pos: vscode.Position = new vscode.Position(21, 11);
+        const acceptableLines: Set<number> = new Set([21, 28, 29]);
+        vscode.commands.executeCommand("vscode.executeReferenceProvider", uri, pos).then((references: vscode.Location[]) => {
+            // console.log(`symbol references: ${JSON.stringify(references)}`);
+            assert.equal(references.length, 3);
+            references.forEach((reference: vscode.Location) => {
+                assert.equal(reference.uri.path, filepath);
+                assert.ok(reference.range.isSingleLine);
+                assert.ok(acceptableLines.has(reference.range.start.line));
+                assert.equal(reference.range.start.character, 9);
+            });
+            done();
         });
     });
 
     test("wildcard references", function (done) {
         const filepath: string = path.join(workspace, "peek_rules.yara");
-        vscode.workspace.openTextDocument(filepath).then(function (doc) {
-            const refProvider: vscode.ReferenceProvider = new YaraReferenceProvider();
-            // $hex_*: Line 31, Col 11
-            const pos: vscode.Position = new vscode.Position(30, 11);
-            // console.log(`search term: ${doc.getText(doc.getWordRangeAtPosition(pos))}`);
-            const ctx: vscode.ReferenceContext | null = null;
-            const tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
-            const results = refProvider.provideReferences(doc, pos, ctx, tokenSource.token);
-            let passed = true;
-            const acceptableLines: Set<number> = new Set([19, 20, 24]);
-            if (results instanceof Array && results.length == 3) {
-                results.forEach(reference => {
-                    if (!acceptableLines.has(reference.range.start.line)) { passed = false; }
-                });
-                if (passed) { done(); }
-            }
-            else if (results instanceof Promise) {
-                results.then(function (references) {
-                    if (references.length != 3) {
-                        passed = false;
-                    }
-                    else {
-                        references.forEach(reference => {
-                            if (!acceptableLines.has(reference.range.start.line)) { passed = false; }
-                        });
-                        if (passed) { done(); }
-                    }
-                });
-            }
+        const uri: vscode.Uri = vscode.Uri.file(filepath);
+        // $hex_*: Line 31, Col 11
+        const pos: vscode.Position = new vscode.Position(30, 11);
+        const acceptableLines: Set<number> = new Set([19, 20, 24]);
+        vscode.commands.executeCommand("vscode.executeReferenceProvider", uri, pos).then((references: vscode.Location[]) => {
+            // console.log(`wildcard references: ${JSON.stringify(references)}`);
+            assert.equal(references.length, 3);
+            references.forEach((reference: vscode.Location) => {
+                assert.equal(reference.uri.path, filepath);
+                assert.ok(reference.range.isSingleLine);
+                assert.ok(acceptableLines.has(reference.range.start.line));
+                assert.equal(reference.range.start.character, 9);
+            });
+            done();
         });
     });
 
@@ -145,14 +90,20 @@ suite("YARA: Provider", function () {
         const uri: vscode.Uri = vscode.Uri.file(filepath);
         // "cuckoo.": Line 8, Col 12
         const pos: vscode.Position = new vscode.Position(9, 12);
-        vscode.commands.executeCommand('vscode.executeCompletionItemProvider', uri, pos, '.').then((items: vscode.CompletionList) => {
-            if (items.items[0].label == "network" || items.items[0].kind == vscode.CompletionItemKind.Class &&
-                items.items[1].label == "registry" || items.items[1].kind == vscode.CompletionItemKind.Class &&
-                items.items[2].label == "filesystem" || items.items[2].kind == vscode.CompletionItemKind.Class &&
-                items.items[3].label == "sync" || items.items[3].kind == vscode.CompletionItemKind.Class) {
-                done();
-            }
+        vscode.commands.executeCommand('vscode.executeCompletionItemProvider', uri, pos, '.').then((completions: vscode.CompletionList) => {
+            console.log(`code completion: ${JSON.stringify(completions)}`);
+            assert.equal(completions.isIncomplete, false);
+            assert.equal(completions.items.length, 4);
+            assert.equal(completions.items[0].label, "network");
+            assert.equal(completions.items[0].kind, vscode.CompletionItemKind.Class);
+            assert.equal(completions.items[1].label, "registry");
+            assert.equal(completions.items[1].kind, vscode.CompletionItemKind.Class);
+            assert.equal(completions.items[2].label, "filesystem");
+            assert.equal(completions.items[2].kind, vscode.CompletionItemKind.Class);
+            assert.equal(completions.items[3].label, "sync");
+            assert.equal(completions.items[3].kind, vscode.CompletionItemKind.Class);
         });
+        done();
     });
 
     /*
@@ -165,39 +116,26 @@ suite("YARA: Provider", function () {
     */
     test("issue #17", function (done) {
         const filepath: string = path.join(workspace, "peek_rules.yara");
-        vscode.workspace.openTextDocument(filepath).then(function (doc) {
-            const refProvider: vscode.ReferenceProvider = new YaraReferenceProvider();
-            // $hex_string: Line 20, Col 11
-            const pos: vscode.Position = new vscode.Position(19, 11);
-            // console.log(`search term: ${doc.getText(doc.getWordRangeAtPosition(pos))}`);
-            const ctx: vscode.ReferenceContext | null = null;
-            const tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
-            const results = refProvider.provideReferences(doc, pos, ctx, tokenSource.token);
-            let passed = true;
-            const acceptableLines: Set<number> = new Set([19, 24, 39, 41]);
-            if (results instanceof Array && results.length == 4) {
-                results.forEach(reference => {
-                    const refWordRange: vscode.Range = doc.getWordRangeAtPosition(reference.range.start);
-                    const refWord: string = doc.getText(refWordRange);
-                    if (refWord != "hex_string" && acceptableLines.has(reference.range.start.line)) { passed = false; }
-                });
-                if (passed) { done(); }
-            }
-            else if (results instanceof Promise) {
-                results.then(function (references) {
-                    if (references.length != 4) {
-                        passed = false;
-                    }
-                    else {
-                        references.forEach(reference => {
-                            const refWordRange = doc.getWordRangeAtPosition(reference.range.start);
-                            const refWord: string = doc.getText(refWordRange);
-                            if (refWord != "hex_string" && acceptableLines.has(reference.range.start.line)) { passed = false; }
-                        });
-                    }
-                    if (passed) { done(); }
-                });
-            }
+        const uri: vscode.Uri = vscode.Uri.file(filepath);
+        // $hex_string: Line 20, Col 11
+        const pos: vscode.Position = new vscode.Position(19, 11);
+        const acceptableLines: Set<number> = new Set([19, 24, 40, 42]);
+        vscode.commands.executeCommand("vscode.executeReferenceProvider", uri, pos).then((references: vscode.Location[]) => {
+            // console.log(`issue 17: ${JSON.stringify(references)}`);
+            assert.equal(references.length, 4);
+            references.forEach((reference: vscode.Location) => {
+                assert.equal(reference.uri.path, filepath);
+                assert.ok(reference.range.isSingleLine);
+                assert.ok(acceptableLines.has(reference.range.start.line));
+                if (reference.range.start.line == 42) {
+                    // "SyntaxExample and $hex_string"
+                    assert.equal(reference.range.start.character, 27);
+                }
+                else {
+                    assert.equal(reference.range.start.character, 9);
+                }
+            });
+            done();
         });
     });
 
@@ -207,27 +145,19 @@ suite("YARA: Provider", function () {
     */
    test("issue #32", function (done) {
         const filepath: string = path.join(workspace, "private_rule_goto.yara");
-        vscode.workspace.openTextDocument(filepath).then(function (doc) {
-            const defProvider: vscode.DefinitionProvider = new YaraDefinitionProvider();
-            // my_private_rule: Line 10, Col 14
-            const pos: vscode.Position = new vscode.Position(9, 14);
-            const tokenSource: vscode.CancellationTokenSource = new vscode.CancellationTokenSource();
-            const result = defProvider.provideDefinition(doc, pos, tokenSource.token);
-            if (result instanceof vscode.Location) {
-                const resultWordRange: vscode.Range = doc.getWordRangeAtPosition(result.range.start);
-                const resultWord: string = doc.getText(resultWordRange);
-                if (resultWord == "my_private_rule" && resultWordRange.start.line == 0 && resultWordRange.start.character == 13) { done(); }
-            }
-            else if (result instanceof Array) {
-                // Should only get one result, so we've failed if an Array is returned
-            }
-            else if (result instanceof Promise) {
-                result.then(function (definition: vscode.Location) {
-                    const resultWordRange: vscode.Range = doc.getWordRangeAtPosition(definition.range.start);
-                    const resultWord: string = doc.getText(resultWordRange);
-                    if (resultWord == "my_private_rule" && resultWordRange.start.line == 0 && resultWordRange.start.character == 13) { done(); }
-                });
-            }
+        const uri: vscode.Uri = vscode.Uri.file(filepath);
+        // my_private_rule: Line 10, Col 14
+        const pos: vscode.Position = new vscode.Position(9, 14);
+        vscode.commands.executeCommand("vscode.executeDefinitionProvider", uri, pos).then((references: vscode.Location[]) => {
+            // console.log(`issue 32: ${JSON.stringify(references)}`);
+            assert.equal(references.length, 1);
+            references.forEach((reference: vscode.Location) => {
+                assert.equal(reference.uri.path, filepath);
+                assert.ok(reference.range.isSingleLine);
+                assert.equal(reference.range.start.line, 0);
+                assert.equal(reference.range.start.character, 13);
+            });
+            done();
         });
    });
 });
