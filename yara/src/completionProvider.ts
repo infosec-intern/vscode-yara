7371b24e4cda4ab334a17418bd234a81a03cd979
infosec-intern <thomas@infosec-intern.com>
2021-03-22 09:59:58 -0600

    Improved completionitem display and resolve

--- yara/src/completionProvider.ts
+++ yara/src/completionProvider.ts
@@ -37,14 +37,17 @@ function getCompletionItemKind(schemaType: string): vscode.CompletionItemKind {
     else if (schemaType === 'property') {
         kind = vscode.CompletionItemKind.Property;  // 9
     }
-    else if (schemaType === 'struct') {
-        kind = vscode.CompletionItemKind.Struct;    // 21
-    }
+    // else if (schemaType === 'field') {
+    //     kind = vscode.CompletionItemKind.Field;    // 4
+    // }
     else if (schemaType === 'array') {
         kind = vscode.CompletionItemKind.Unit;      // 10
     }
+    else if (schemaType === 'dictionary') {
+        kind = vscode.CompletionItemKind.Struct;    // 21
+    }
     else {
-        console.log(`got schema I don't recognize: ${JSON.stringify(schemaType)}`);
+        // console.log(`got schema I don't recognize: ${JSON.stringify(schemaType)}`);
     }
     return kind;
 }
@@ -53,17 +56,22 @@ function getCompletionItemKind(schemaType: string): vscode.CompletionItemKind {
     Generate a CompletionItem object out of a given label and kind
     ... and potentially transform it based on its kind
 */
-function makeCompletionItem(schemaLabel: string, schemaKind: string): vscode.CompletionItem|undefined {
-    let item: vscode.CompletionItem|undefined = undefined;
-    const itemKind: vscode.CompletionItemKind = getCompletionItemKind(schemaKind);
-    if (itemKind === vscode.CompletionItemKind.Method) {
-        item = new vscode.CompletionItem(`${schemaLabel}()`, itemKind);
-    }
-    else {
-        item = new vscode.CompletionItem(schemaLabel, itemKind);
-    }
-    return item;
-}
+// function makeCompletionItem(schemaLabel: string, schemaKind: string): vscode.CompletionItem|undefined {
+//     let item: vscode.CompletionItem|undefined = undefined;
+//     const itemKind: vscode.CompletionItemKind = getCompletionItemKind(schemaKind);
+//     if (itemKind === vscode.CompletionItemKind.Method) {
+//         item = new vscode.CompletionItem(`${schemaLabel}`, itemKind);
+//         item.insertText = new vscode.SnippetString(`${schemaLabel}($1)`);
+//     }
+//     else if (itemKind === vscode.CompletionItemKind.Struct) {
+//         item = new vscode.CompletionItem(`${schemaLabel}`, itemKind);
+//         item.insertText = new vscode.SnippetString(`${schemaLabel}["$\{1:key}"]`);
+//     }
+//     else {
+//         item = new vscode.CompletionItem(schemaLabel, itemKind);
+//     }
+//     return item;
+// }
 
 /*
     Convert a directory of module JSON schemas to Module objects
@@ -84,20 +92,21 @@ function parseSchema(schemaPath: string): ModuleSchema {
                 // possibly includes sub-fields on each array entry (e.g. pe.sections[].name)
                 itemKind.forEach((property: Map<string,string>) => {
                     Object.keys(property).forEach((subField: string) => {
-                        item = makeCompletionItem(`${moduleName}.${attribute}[].${subField}`, property[subField]);
+                        item = new vscode.CompletionItem(`${moduleName}.${attribute}[].${subField}`, getCompletionItemKind(property[subField]));
                         yaraModule.push(item);
                     });
                 });
             }
             else if (typeof itemKind === 'string') {
                 // simple module fields (e.g. pe.number_of_sections, hash.md5)
-                item = makeCompletionItem(`${moduleName}.${attribute}`, itemKind);
+                item = new vscode.CompletionItem(`${moduleName}.${attribute}`, getCompletionItemKind(itemKind));
                 yaraModule.push(item);
             }
             else if (typeof itemKind === 'object') {
                 // module fields with their own sub-fields (e.g. cuckoo.network.http_request)
                 Object.keys(itemKind).forEach((subField: string) => {
-                    item = makeCompletionItem(`${moduleName}.${attribute}.${subField}`, itemKind[subField]);
+                    item = new vscode.CompletionItem(`${moduleName}.${attribute}.${subField}`, getCompletionItemKind(itemKind[subField]));
+                    // item = makeCompletionItem(`${moduleName}.${attribute}.${subField}`, itemKind[subField]);
                     yaraModule.push(item);
                 });
             }
@@ -128,7 +137,18 @@ export class YaraCompletionItemProvider implements vscode.CompletionItemProvider
                             return entry.label.startsWith(fullTerm);
                         }).map<vscode.CompletionItem>((entry: vscode.CompletionItem) => {
                             // remove any leading characters that overlap with the term already in the document
-                            entry.insertText = entry.label.replace(fullTerm, '');
+                            // ... but keep full module path as a detail item and the text to filter by
+                            entry.detail = entry.label;
+                            entry.filterText = entry.label;
+                            console.log(terms);
+                            const insertText = entry.label.replace(fullTerm, '');
+                            if (entry.kind === vscode.CompletionItemKind.Method) {
+                                entry.label = `${entry.label}()`;
+                                entry.insertText = new vscode.SnippetString(`${insertText}($1)`);
+                            }
+                            else if (entry.kind === vscode.CompletionItemKind.Struct) {
+                                entry.insertText = new vscode.SnippetString(`${insertText}["$\{1:key}"]`);
+                            }
                             return entry;
                         });
                         resolve(items);
@@ -145,6 +165,7 @@ export class YaraCompletionItemProvider implements vscode.CompletionItemProvider
     public resolveCompletionItem(item: vscode.CompletionItem, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CompletionItem> {
         return new Promise((resolve) => {
             token.onCancellationRequested(resolve);
+            console.log(item);
             resolve(item);
         });
     }
